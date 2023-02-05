cb22ccc7f8e9fea1393b23dd4c68e9d37a5dacdb
antant200 <antonmarkelov20020605@yandex.ru>
2023-02-05 18:36:07 +0700

    Добавлен класс Main

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] sales = {10, 115, 4, 1101};
+        SalesManager salesManager = new SalesManager(sales);
+        System.out.println(salesManager.max());
+    }
+}
+
+

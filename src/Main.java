89e3a17ba5109baa3539fb62e2aebb4072960cf8
antant200 <antonmarkelov20020605@yandex.ru>
2023-02-05 18:40:14 +0700

    Поменял int на long

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] sales = {10, 115, 4, 1101};
+        long[] sales = {10, 115, 4, 1101};
         SalesManager salesManager = new SalesManager(sales);
         System.out.println(salesManager.max());
     }
